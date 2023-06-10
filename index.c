6ba6d5f91cb5f5d84b6f3ae4a00892a2850d05f9
TheusMarqs <matheusmarqsilva@gmail.com>
2023-06-10 17:26:20 -0300

    exercicio 10

--- index.c
+++ index.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    int i;
+    double valores[6];
+    int i, positivos = 0;
+    double soma = 0.0;
 
-    for (i = 1; i <= 100; i++) {
-        if (i % 2 == 0) {
-            printf("%d\n", i);
+    // Leitura dos valores
+    for (i = 0; i < 6; i++) {
+        scanf("%lf", &valores[i]);
+        if (valores[i] > 0) {
+            positivos++;
+            soma += valores[i];
         }
     }
+
+    // Impressão da quantidade de valores positivos
+    printf("%d valores positivos\n", positivos);
+
+    // Cálculo e impressão da média dos valores positivos
+    printf("%.1lf\n", soma / positivos);
 
     return 0;
 }
