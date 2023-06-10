b0548307212b5008020f74f4af78178e12b0cdb1
TheusMarqs <matheusmarqsilva@gmail.com>
2023-06-10 17:06:52 -0300

    exercicio 2

--- index.c
+++ index.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main()
-{
-    printf("Hello World!\n");
+int main() {
+    int A, B, X;
+
+    // Leitura dos valores de A e B
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    // Soma de A e B
+    X = A + B;
+
+    // Impressão do resultado
+    printf("X = %d\n", X);
+
     return 0;
 }
