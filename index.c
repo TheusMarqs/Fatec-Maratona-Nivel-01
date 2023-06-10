ebf0f6158fc99ad90a9a8fb7d392ddab8130b368
TheusMarqs <matheusmarqsilva@gmail.com>
2023-06-10 17:39:39 -0300

    exercicio 19

--- index.c
+++ index.c
@@ -3,15 +3,12 @@
 int main() {
     int i, j;
 
-    // Inicialização das variáveis
-    i = 1;
-    j = 60;
-
-    // Loop para imprimir a sequência
-    while (j >= 0) {
-        printf("I=%d J=%d\n", i, j);
-        i += 3;
-        j -= 5;
+    // Loop externo para a variável i
+    for (i = 1; i <= 9; i += 2) {
+        // Loop interno para a variável j
+        for (j = 7; j >= 5; j--) {
+            printf("I=%d J=%d\n", i, j);
+        }
     }
 
     return 0;
